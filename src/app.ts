2e5e9ed9a86c7209473ff10bd6a035d90abaeb6e
Kuvondik-95 <kuvondik9595@gmail.com>
2025-07-05 21:01:47 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,18 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+import { T } from './libs/types/common';
+
+
+// TCP lightWeigth connection qurib olyapmiz session lar uchun
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore ({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions"
+});
+
 /** 1-ENTRANCE **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,9 +25,18 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2-SESSIONS **/
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+    },
+    store: store,
+    resave: true,  
+    saveUninitialized: true 
+}));
 
 /** 3-VIEWS **/
-console.log("__dirname:", __dirname);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
