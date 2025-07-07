67c7ace5a8ab8e94f553a04e18bf148b77f912a5
Kuvondik-95 <kuvondik9595@gmail.com>
2025-07-08 01:27:23 +0900

    feat: update user status frontend logic and SPA login

--- src/models/member.service.ts
+++ src/models/member.service.ts
@@ -2,7 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput, MemberUpdateInput } from "../libs/types/member";
 import Errors, { Message } from "../libs/Errors";
 import { HttpCode } from "../libs/Errors";
-import { MemberType } from "../libs/enums/member.enum";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 
@@ -30,40 +30,28 @@ class MemberService{
   }
   
   public async login(input: LoginInput): Promise<Member>{
-    // TODO: Consider member status later
     const member = await this.memberModel
-      .findOne(
-        { memberNick: input.memberNick },
-        { memberNick: 1, memberPassword: 1 }
-      )
-      .exec();
-    if(!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
-
+    // TODO consider member status later
+    .findOne({ 
+      memberNick: input.memberNick,
+      memberStatus: { $ne: MemberStatus.DELETE }
+    }, 
+      {memberNick: 1, memberStatus: 1, memberPassword: 1, })
+    .exec()
+    if(!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK)
+    else if(member.memberStatus === MemberStatus.BLOCK)
+      throw new Errors(HttpCode.FORBIDDEN, Message.BLOCKED_USER);
     const isMatch = await bcrypt.compare(
-      input.memberPassword,
+      input.memberPassword, 
       member.memberPassword
-    );
+    )
+
     if(!isMatch) throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
     
-    return this.memberModel.findById(member._id).lean().exec();
+    return await this.memberModel.findById(member._id).lean().exec(); 
   }
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
   /** SSR **/
   public async processSignup(input: MemberInput): Promise<Member>{
     const exist = await this.memberModel
