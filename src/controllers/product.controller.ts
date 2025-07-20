b1a5e5206192ea216017d34ff7dbc07b8053c809
Kuvondik-95 <kuvondik9595@gmail.com>
2025-07-20 19:22:28 +0500

    feat: create getProduct Rest API

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -34,20 +34,20 @@ productController.getProducts = async (req: Request, res: Response) => {
 }
 
 
-// productController.getProduct = async (req: ExtendedRequest, res: Response) => {
-//   try{
-//     console.log("getProduct");
-//     const { id } = req.params;
-//     const memberId = req.member?._id ?? null,
-//       result = await productService.getProduct(memberId, id);
+productController.getProduct = async (req: ExtendedRequest, res: Response) => {
+  try{
+    console.log("getProduct");
+    const { id } = req.params;
+    const memberId = req.member?._id ?? null,
+      result = await productService.getProduct(memberId, id);
 
-//     res.status(HttpCode.OK).json(result);
-//   }catch(err){
-//     console.log("Error, getProduct:", err);
-//     if(err instanceof Errors) res.status(err.code).json(err);
-//     else res.status(Errors.standard.code).json(Errors.standard);
-//   }
-// }
+    res.status(HttpCode.OK).json(result);
+  }catch(err){
+    console.log("Error, getProduct:", err);
+    if(err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+}
 
 
 
