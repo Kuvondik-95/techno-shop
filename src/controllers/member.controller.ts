44b29f64d72e8a8543972e7f752350172c060315
Kuvondik-95 <kuvondik9595@gmail.com>
2025-07-20 17:19:28 +0500

    feat: create middleware methods and logout member process

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -54,6 +54,18 @@ memberController.login = async (req: Request, res: Response) => {
   }
 };
 
+memberController.logout = (req: ExtendedRequest, res: Response) => {
+  try{
+    console.log("logout");
+    res.cookie("accessToken", null, {maxAge: 0, httpOnly: true});
+    res.status(HttpCode.OK).json({ logout: true });
+  }catch(err){
+    console.log("Error, logout:", err);
+    if(err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
 memberController.verifyAuth = async (
   req: ExtendedRequest, 
   res: Response,
@@ -64,12 +76,26 @@ memberController.verifyAuth = async (
 
     if(!req.member) throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
 
-    // next();
-    res.status(HttpCode.OK).json({ member: req.member});
+    next();
   }catch(err){
     console.log("Error, verifyAuth:", err);
     if(err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+memberController.retrieveAuth = async (
+  req: ExtendedRequest, 
+  res: Response,
+  next: NextFunction) => {
+  try{
+    const token = req.cookies["accessToken"];
+    if(token) req.member = await authService.checkAuth(token);
+
+    next();
+  }catch(err){
+    console.log("Error, retrieveAuth:", err);
+    next()
   }
 };
 
